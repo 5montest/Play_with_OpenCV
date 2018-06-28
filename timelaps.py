b6b4ce9f931ef1baadcd859166efd7ad6ef4cc24
Your Name <you@example.com>
2018-06-28 14:49:43 +0900

    bug fixs

--- timelaps.py
+++ timelaps.py
@@ -1,45 +1,53 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*-coding: utf-8 -*-
 
 import cv2
 import time
 
 cnt = 1
+cap = cv2.VideoCapture(0)
+
+print("Please set lap time:")
+laptime = input()
+laptime = int(laptime)
+
+print("Please set flamerate:")
+fps = input()
+fps = int(fps)
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+video = cv2.VideoWriter("data/timelaps.avi",fourcc,fps,(640,480))
 
 def main():
     first = 0
-    fourcc = cv2.VideoWriter_fourcc('I','Y','U','V')
-    video = cv2.VideoWriter("data/timelaps.avi",fourcc,20.0,(640,480))
-
-    print("Please set lap time:")
-    laptime = input()
-    laptime = int(laptime)
     
     global cnt
 
     while True:
         cnt = int(cnt)
-        if first > 1:
+        if first == 1:
             time.sleep(laptime)
 
             ret, frame = cap.read()
             if ret:
-                cv2.imwrite("data/image"+str(cnt)+".png",frame)
+                cv2.imwrite("data/image"+str(cnt)+".jpg",frame)
                 cnt += 1
         else:
             first = 1
-            cap = cv2.VideoCapture(0)
             ret, frame = cap.read()
             if ret:
-                cv2.imwrite("data/image"+str(cnt)+".png",frame)
+                cv2.imwrite("data/image"+str(cnt)+".jpg",frame)
                 cnt += 1
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        for i in range(1,20):
-            img = cv2.imread("data/image"+str(cnt)+".png",format(i))
-            img = cv2.resize(img,(640,480))
+        mai = 1
+        for i in range(1,cnt):
+            img = cv2.imread("data/image"+str(mai)+".jpg")
+            #img = cv2.resize(img,(640,480))
             video.write(img)
+            mai += 1
+        print("done")
         video.release()
